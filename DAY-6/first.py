c267f754d1f129db5df7f37331e1391ad15ed0c5
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-03-17 16:01:12 +0530

    The functionality worked | Sort Of

--- DAY-6/first.py
+++ DAY-6/first.py
@@ -10,32 +10,32 @@ count2 = 0
 count1 = 0
 
 
-if(amt % 2000 == 0):
-    count2000 += 1
-    amt -= 2000
-elif ( amt % 500 == 0):
-    count500 += 1
-    amt -= 500
-elif ( amt % 200 == 0):
-    count200 += 1
-    amt -= 200
-elif ( amt %100 == 0):
-    count100 +=1
-    amt -= 100
-elif ( amt % 50 == 0):
-    count50 += 1
-    amt -= 50
-elif ( amt % 10 == 0):
-    count500 += 1
-    amt -= 10
-elif ( amt % 5 == 0):
-    count5 += 1
-    amt -= 5
-elif ( amt % 2 == 0):
-    count2 += 1
-    amt -= 2
-elif ( amt % 1 == 0):
-    count1 += 1
-    amt -= 1
+if amt >= 2000:
+    count2000 = amt // 2000
+    amt -= count2000*2000
+if amt >=500:
+    count500 = amt //500
+    amt -= count500 *500
+if amt >=200:
+    count200 = amt // 200
+    amt -= count200*200
+if amt >=100:
+    count100 = amt // 100
+    amt -= count100 *100
+if amt >=50:
+    count50 = amt //50
+    amt -= count50 * 50
+if amt >=10:
+    count500 = amt //10
+    amt -= count10 * 10
+if amt >=5:
+    count5 = amt // 5
+    amt -= count5 * 5
+if amt >=2:
+    count2 = amt //2
+    amt -= count2 * 2
+if amt >=1:
+    count1 = amt // 1
+    amt -= count1 * 1
 
 print(count2000, count500, count200, count100, count50, count10, count5, count2, count1)

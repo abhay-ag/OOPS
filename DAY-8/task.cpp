665a6ad2d6f60377aac01241597ded630cc367d9
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-03-22 15:47:59 +0530

    Task-3 Done

--- DAY-8/task.cpp
+++ DAY-8/task.cpp
@@ -6,16 +6,20 @@ int main(){
     int i, j, f;
     int bigNum = -9999;
     int smallNum = 9999;
+    int count;
 
     int arr[10];
     int arrMaxMin[10];
+    int arrEven[10];
+
+    for(i = 0; i<10; i++){
+        cout << "Enter the elements: "; 
+        cin >> arr[i];
+    }
 
     cout << "Enter the element to find in array: ";
     cin >> f;
 
-    for(i = 0; i<10; i++){
-        cin >> arr[i];
-    }
     for(j = 0; j<10; j++){
         if (f == arr[j]){
             cout << "Element found at index: " << j << endl;
@@ -39,5 +43,19 @@ int main(){
 
     cout << "Max in Array is: " << bigNum << endl;
     cout << "Min in Array is: " << smallNum << endl;
+
+    // Program 3 --> Count even nos
+
+    for (i = 0; i < 10; i++){
+        cout << "Enter the elements: "; 
+        cin >> arrEven[i];
+    }
+
+    for (j = 0; j < 10; j++){
+        if(arrEven[j] % 2 == 0){
+            count +=1;
+        }
+    }
+    cout << "Count of even nos in array is: " << count << endl;
     return 0;
 }

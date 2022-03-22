c8a677574fcb3053e21b5d56d7324bb939a6a1fc
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-03-22 15:38:55 +0530

    Task-2 Completed

--- DAY-8/task.cpp
+++ DAY-8/task.cpp
@@ -4,8 +4,11 @@ using namespace std;
 int main(){
     // Program 1 --> Finding an element from array
     int i, j, f;
+    int bigNum = -9999;
+    int smallNum = 9999;
 
     int arr[10];
+    int arrMaxMin[10];
 
     cout << "Enter the element to find in array: ";
     cin >> f;
@@ -18,5 +21,23 @@ int main(){
             cout << "Element found at index: " << j << endl;
         }
     }
+
+    // Program 2 --> Finding max and min
+
+    for(i = 0; i < 10; i ++){
+        cout << "Enter the elements: "; 
+        cin >> arrMaxMin[i];
+    }
+    for (j = 0; j<10; j++){
+        if(arrMaxMin[j] > bigNum){
+            bigNum = arrMaxMin[j];
+        }
+        if(arrMaxMin[j] < smallNum){
+            smallNum = arrMaxMin[j];
+        }
+    }
+
+    cout << "Max in Array is: " << bigNum << endl;
+    cout << "Min in Array is: " << smallNum << endl;
     return 0;
 }

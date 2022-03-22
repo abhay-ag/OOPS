08174dc97752db7a9018b89dad246a0c4a6b9f28
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-03-22 16:10:33 +0530

    Task -6 Completed

--- DAY-8/task2.cpp
+++ DAY-8/task2.cpp
@@ -3,9 +3,21 @@
 using namespace std;
 
 int main(){
+    // Sorting an array
     int arr[6] = {5,3,2,4,1,5};
 
-    int i;
-
+    int i,j,third;
+    for(i = 0; i<6; i++){
+        for(j = i+1; j<6; j ++){
+            if (arr[i] > arr[j]){
+                third = arr[i];
+                arr[i] = arr[j];
+                arr[j] = third;
+            }
+        }
+    }
+    for(i = 0; i<6; i++){
+        cout << arr[i] << endl;
+    }
     return 0;
 }

687d2277c5a488a6bb2aa1f6e1bc0ba715e21dee
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-03-22 15:34:14 +0530

    Task 1 Completed

--- DAY-8/task.cpp
+++ DAY-8/task.cpp
@@ -2,7 +2,21 @@
 
 using namespace std;
 int main(){
-    
+    // Program 1 --> Finding an element from array
+    int i, j, f;
 
+    int arr[10];
+
+    cout << "Enter the element to find in array: ";
+    cin >> f;
+
+    for(i = 0; i<10; i++){
+        cin >> arr[i];
+    }
+    for(j = 0; j<10; j++){
+        if (f == arr[j]){
+            cout << "Element found at index: " << j << endl;
+        }
+    }
     return 0;
 }

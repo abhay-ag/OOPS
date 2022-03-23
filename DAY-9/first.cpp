d9040ebd4e42c941ba7131b9a21202fc0a1c02de
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-03-23 15:35:51 +0530

    About Pointers

---
+++ DAY-9/first.cpp
@@ -0,0 +1,10 @@
+/*
+    Pointers
+    ________
+    Every variable has an address in the memory which it is stored.
+    POINTER Variables ==> the variables that store the address of another variable in the memory
+
+        Eg: int a ==> 'a' stores 4 bytes in the memory and it has a name
+            1000 -> 1004 memory block for a
+            make a variable ptr to store the address of 'a' ==> Pointer Variable [Carrying the address of a]
+*/

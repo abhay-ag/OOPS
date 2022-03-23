ba65a8de2afb2676fcb4e549215504855b20e638
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-03-23 15:37:42 +0530

    Declaring Pointers

--- DAY-9/first.cpp
+++ DAY-9/first.cpp
@@ -8,3 +8,16 @@
             1000 -> 1004 memory block for a
             make a variable ptr to store the address of 'a' ==> Pointer Variable [Carrying the address of a]
 */
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b;
+    a = 20; b = 30;
+    /*
+        Declaration of Pointer Variable
+            <datatype> * <name of variable>
+    */
+    return 0;
+}

d2f188abf1ec73c7c40599a0155c525e129ece30
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-03-25 11:59:13 +0530

    Functions

---
+++ DAY-10/first.cpp
@@ -0,0 +1,9 @@
+/*  
+    FUNCTIONS:
+        set of statements written separately from main to perform a particular task.
+        Purpose of Functions:
+            To ease the process of writing code as same code can be used again and again
+
+    Definition of a function:  
+        
+*/

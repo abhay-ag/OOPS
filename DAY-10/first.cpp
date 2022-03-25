d286061b70782d7ba0fc92d70d2ed1d68eea05c0
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-03-25 12:03:44 +0530

    Syntax of function

--- DAY-10/first.cpp
+++ DAY-10/first.cpp
@@ -5,5 +5,8 @@
             To ease the process of writing code as same code can be used again and again
 
     Definition of a function:  
-        
+        <return type> <name of the function> ([parameters with data types if any]){
+            [body];
+            return; // If a function returns any value then use a return type else use void;
+        }
 */

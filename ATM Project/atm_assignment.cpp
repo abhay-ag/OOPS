ca57005b51936ed72f888df20c4b17edd4e649d4
abhay_ag <abhay0034.be21@chitkara.edu.in>
2022-03-26 20:03:23 +0530

    Some of Login Completed | Create Account Functionality Half Done

--- ATM Project/atm_assignment.cpp
+++ ATM Project/atm_assignment.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
-char menuInput, mainInput;
-vector<int> balance;
-void printIntroMenu();
+char menuInput;
+char mainInput;
+vector<string> id;
+vector<string> password;
+void printIntroMenu(char menuInput);
 void printMainMenu();
 void start();
 void login();
 void createAccount();
+void addData(string name, string pass);
 
 int main(){
     cout << "Hello, User!! Welcome to this ATM Project!" << endl;
@@ -19,33 +23,65 @@ int main(){
 }
 void start(){
     cout << "Please Select an option from the menu below: " << endl;
-    printIntroMenu();
-}
-
-void printIntroMenu(){
     cout << "l -> Login\nc -> Create New Account\nq -> Quit\n\n> " ;
     cin >> menuInput;
+    printIntroMenu(menuInput);
+}
 
+void printIntroMenu(char menuInput){
     switch (menuInput){
         case 'l':
-            // login();
+            login();
             break;
         case 'c':
-            // createAccount();
+            createAccount();
             break;
         case 'q':
             exit(0);
             break;
-        default:
-            cout << "Please enter correct input!!! \n" << endl;
-            printIntroMenu();
-            break;
+        // default:
+        //     cout << "Please enter correct input!!! \n" << endl;
+        //     printIntroMenu();
+        //     break;
     }
+}
+
+void login(){
+    if (id.size() == 0 && password.size()  == 0){
+        cout << "Currently we have no accounts!! Please create an accout!" << endl << endl;
+        createAccount();
+    }
+}
+
+void addData(string name, string pass){
+    replace(name.begin(), name.end(), ' ', '_');
+    id.push_back(name);
+    replace(pass.begin(), pass.end(), ' ', '_');
+    password.push_back(pass);
+}
+
+void createAccount(){
+    string uname, paswd, dummy;
+    getline(cin, dummy);
+    cout << "Enter user name [spaces get converted to underscore]: ";
+    getline(cin, uname);
+
+    cout << endl;
+
+    cout << "Enter a password[spaces get converted to underscore]: ";
+    getline(cin, paswd);
+
+    addData(uname, paswd);
+
+    cout << "Thank You! Your account has been created!" << endl << endl;
+    
+    cout << "l -> Login\nc -> Create New Account\nq -> Quit\n\n> " ;
+    cin >> menuInput;
+
+    printIntroMenu(menuInput);
 }
 
 void printMainMenu(){
     cout << "d -> Deposit Money\nw -> Withdraw Money\nr -> Request Money\nq -> Quit\n\n> " ;
     cin >> mainInput;
-
-    
 }

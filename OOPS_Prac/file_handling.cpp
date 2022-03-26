853e3005bbc36314aa0098b5e0ae22db45e9c9d7
abhay_ag <abhay0034.be21@chitkara.edu.in>
2022-03-26 17:27:53 +0530

    Wrote some names to the csv File

--- OOPS_Prac/file_handling.cpp
+++ OOPS_Prac/file_handling.cpp
@@ -10,6 +10,8 @@ using namespace std;
 int main(){
     ofstream mef;               // Opening a file if it doesn't exist it will be created
     mef.open("test.csv");
-    mef << "Abhay, Aggarwal"  << endl;      // Values are separated by commas, and a new line character is inserted at the end to enter a new row in the csv file
+    mef << "Hello, Kitty"  << endl;      // Values are separated by commas, and a new line character is inserted at the end to enter a new row in the csv file
+    mef << "Michael, Jackson" << endl;
+    mef << "Prabh, Deep" << endl;
     mef.close();                // Closing a file is necessary and has to be donee ohterwise it results in error in the future
 }

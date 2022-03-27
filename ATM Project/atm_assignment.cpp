2ce7a7fb3828e51a04d5ddddd46d5a62d1665336
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-03-27 10:17:17 +0530

    Introduced Comments | Some Bugs found

--- ATM Project/atm_assignment.cpp
+++ ATM Project/atm_assignment.cpp
@@ -10,12 +10,26 @@
 
 // https://stackoverflow.com/questions/5131647/why-would-we-call-cin-clear-and-cin-ignore-after-reading-input
 
+/*
+    Pre-Defining all the functions to be used in the program.
+    All global variables declared here.
+
+    Project Started on: 26th March, 2022;
+    project Finished on: 27th March, 2022;
+*/
+
 using namespace std;
 char menuInput;
 char mainInput;
+
+// Vectors support dynamic assignment so used over arrays in this project
+
 vector<string> id;
 vector<string> password;
 vector<int> bal(9999, 0);
+
+// Definition of all functions
+
 void printIntroMenu();
 void printMainMenu(int index, string uname);
 void start();
@@ -26,16 +40,32 @@ void deposit(int index, string uname);
 void withdraw(int index, string uname);
 void request(int index, string uname);
 
+/*
+    The main function just invokes the start function which kicks off the rest of the program;
+
+    Start Function:
+        1. Calls the printIntroMenu() Function to print the entry menu;
+        2. User selects the options shown and the program moves forward accordingly.
+*/
+
 int main(){
     cout << "Hello, User!! Welcome to this ATM Project!" << endl;
     cout << endl;
     start();
     return 0;
 }
+
 void start(){
     cout << "Please Select an option from the menu below: " << endl;
     printIntroMenu();
 }
+
+/*
+    printIntroMenu() function:
+        1. Asks the user for the choice of the function he/she wants to perform
+        2. Proceeds the program accordingly.
+        3. If the user enters wrong choice he/she will be prompted to enter the values again.
+*/
 
 void printIntroMenu(){
     cout << "l -> Login\nc -> Create New Account\nq -> Quit\n\n> " ;
@@ -57,6 +87,16 @@ void printIntroMenu(){
     }
 }
 
+
+/*
+    Login Function:
+        1. This function first check if there are any accounts stored if there is no account the user is prompted to go to the intromenu function to create an account.
+        2. If there is/are account(s) in the storage then the user enters the username.
+        3. the program checks if the username exists before asking for password.
+        4. if the user exists then user is prompted to enter password
+        5. after that password validation occurs and based on that the program proceeds accordingly.
+*/
+
 void login(){
     bool found = false;
     string uname, paswd, dummy;
@@ -65,7 +105,7 @@ void login(){
         cout << endl;
         sleep(1);
         cout << "Currently we have no accounts!! Please create an account!" << endl;
-        createAccount();
+        printIntroMenu();
     }
     else{
         getline(cin, dummy);
@@ -104,12 +144,24 @@ void login(){
     }
 }
 
+/*
+    addData function:
+        1. It justs pushes the passed values into it to the vectors.
+*/
+
 void addData(string name, string pass){
     replace(name.begin(), name.end(), ' ', '_');
     id.push_back(name);
     replace(pass.begin(), pass.end(), ' ', '_');
     password.push_back(pass);
 }
+
+/* 
+
+    Create a Account:
+        1. user is prompted to create an account. By default the spaces get converted to underscores
+
+*/
 
 void createAccount(){
     string uname, paswd, dummy;
@@ -130,6 +182,13 @@ void createAccount(){
 
     printIntroMenu();
 }
+
+/* 
+    printMainMenu funtion:
+        1. It is called if the login is successful
+        2. It asks for the user choice for the functions like deposit, view balance, withdraw.
+        3. the program proceeds further according to the input provided.
+*/
 
 void printMainMenu(int index, string uname){
     cout << endl;
@@ -159,6 +218,11 @@ void printMainMenu(int index, string uname){
     }
 }
 
+/*
+    deposit function:
+        1. Facilitates the user to deposti amount into their account.
+*/
+
 void deposit(int index, string uname){
     cout << endl;
     int dpMoney;
@@ -176,6 +240,14 @@ void deposit(int index, string uname){
 
     printMainMenu(index, uname);
 }
+
+/*
+    withdraw function:
+        1. Checks for the balance in the acount first.
+        2. If balance is 0 then moeny can't be withdrwan.
+        3. Else the control moves further where user is asked for amount to be withdrawn
+        4. the user has to enter amount and if enters more than the balance he/she has to enter again.
+*/
 
 void withdraw(int index, string uname){
     int wdMoney;
@@ -198,16 +270,21 @@ void withdraw(int index, string uname){
         printMainMenu(index, uname);
     }
     else{
+        int i = 0;
         cout << endl;
         cout << "Enter amount to be withdrawn: $";
         cin >> wdMoney;
         while (!(wdMoney <= bal.at(index)))
         {
+            if(i == 2){
+                break;
+            }
             cout << endl;
             sleep(1);
             cout << "You don't have sufficient balance!" << endl << endl;
             cout << "Enter amount to be withdrawn: $";
             cin >> wdMoney;
+            i+=1;
         }
         
         cin.clear();
@@ -221,6 +298,10 @@ void withdraw(int index, string uname){
     }
 }
 
+/*
+    request funtion:
+        1. Displays the user balance.
+*/
 void request(int index, string uname){
     if(bal.at(index) == 0){
         cout << endl;

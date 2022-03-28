d3aa3b41326f66bb1434a9dc9cfee4e8d8548810
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-03-28 11:17:08 +0530

    Swap Function with Value

--- DAY-11/first.cpp
+++ DAY-11/first.cpp
@@ -11,12 +11,27 @@
 
 using namespace std;
 
+void swapValue(int a, int b){
+    int t = a;
+    a = b; 
+    b = t;
+    cout << "Swapped nos. are : " << a << b << endl;
+}
+
 int main(){
     int *ptr;
     int a = 20;
-
     ptr = &a;
-
     cout << ptr << endl;
+
+    swapValue(35,32);
     return 0;
 }
+
+/*
+    Definition of a reference type funtion:
+
+        <return type> <function name> ([arguements with declaration like int *ptr]){
+            //body
+        }
+*/

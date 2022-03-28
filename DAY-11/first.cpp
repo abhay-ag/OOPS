0c88a45ca9a647b19e098e4c8847e723dd549666
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-03-28 11:11:32 +0530

    Recap about pointers | Reference type function

--- DAY-11/first.cpp
+++ DAY-11/first.cpp
@@ -2,4 +2,21 @@
     Functions:
         1. Call by value
         2. Call by refernce
+
+    Call by Value --> add();
+    Call by refernce --> passing the refernce of the variable into the function
 */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int *ptr;
+    int a = 20;
+
+    ptr = &a;
+
+    cout << ptr << endl;
+    return 0;
+}

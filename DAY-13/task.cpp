fbb0f9cfc7c7a859150000d90ce3ae9498784d7e
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-01 12:18:42 +0530

    Biggest in array Bug | Resolved | Task Completed

--- DAY-13/task.cpp
+++ DAY-13/task.cpp
@@ -12,18 +12,27 @@ struct student
 int main(){
     student sArr[5];
     string dummy;
-    int bignum = -9999;
+    int bignum = sArr[0].marks;
+
+    // Problem in assigning the bignum to negative infinity 
+    // Solution :
+        /*
+            Assign bignum to first element of the array;
+            Run the loop from 1;
+        */
 
     for (int i = 0; i < 5 ; i ++){
-        cout << "Enter the Name of student: ";
-        getline(cin, sArr[i].name);
         cout << "Enter the Roll No of student: ";
         cin >> sArr[i].rollNo;
+        getline(cin, dummy);
+        cout << "Enter the Name of student: ";
+        getline(cin, sArr[i].name);
         cout << "Enter the Marks of student: ";
         cin >> sArr[i].marks;
+        cout << endl;
     }
     int index = 0;
-    for (int i = 0; i < 5; i ++){
+    for (int i = 1; i < 5; i ++){
         if (sArr[i].marks > bignum){
             index = i;
             bignum = sArr[i].marks;

ee019d689ff87dcccd84886a33c9b25a9ee1f2b2
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-01 11:58:57 +0530

    Stuctures in C++

---
+++ DAY-13/first.cpp
@@ -0,0 +1,51 @@
+/*
+    Structures:
+        1. User defined data types
+
+    Problem: Store student data (without structures)
+        int main(){
+            int rollNo, marks;
+            char name[50];
+        }
+        HERE ALL THE VARIABLES ARE STORED IN DIFFERENT MEMORY LOCATIONS AND THERE IS NO SUCH INBUILT DATA TYPE WHERE WE CAL STORE IT TOGETHER
+    
+    Solution:
+        USE STRUCTURES
+
+        struct student{
+            int rollNo;
+            char name[20];
+            int marks;
+        }
+
+        'student' will act as a datatype for each of the inner variables.
+
+    SYNTAX:
+        struct <structure name>
+        {
+            // code
+        }
+*/
+
+#include <iostream>
+
+using namespace std;
+
+// Declaring a Structure
+struct student
+{
+    int rollNo;
+    char name[20];
+    int marks;
+};
+// Main Function
+int main(){
+    student s1;         // creating a student object in s1
+
+    /*
+        Accessing elements in a structure
+            <structure name>.data_member
+    */
+
+    return 0;
+}

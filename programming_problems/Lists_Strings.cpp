4dda4f204c98a7b85390833fca6f7337350bba2d
abhay_ag <abhay0034.be21@chitkara.edu.in>
2022-04-03 14:57:29 +0530

    Program 2 completed

--- programming_problems/Lists_Strings.cpp
+++ programming_problems/Lists_Strings.cpp
@@ -12,5 +12,18 @@ int main(){
         }
     }
     cout << "Biggest in array is: " << bigNum << endl;
+
+    // Program 2 ==> Reverse an array
+
+    for ( int i = 0; i < 10; i ++){
+        for (int j = i+1; j < 10; j ++){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j]  = temp;
+        }
+    }
+    for(int i = 0; i < 10; i ++){
+        cout << arr[i] << " ";
+    }
     return 0;
 }

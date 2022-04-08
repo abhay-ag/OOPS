27b56efa152625b45731abe23aeff69095c4f8a1
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-08 11:58:44 +0530

    2-D matrix example

--- DAY-14/first.cpp
+++ DAY-14/first.cpp
@@ -12,5 +12,17 @@
 using namespace std;
 
 int main(){
+    int mat[3][4];
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 4; j++){
+            cin >> mat[i][j];
+        }
+    }
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 4; j++){
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }

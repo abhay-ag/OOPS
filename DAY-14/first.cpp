e0e59029c6c5fd549a69f8f9423f7e244a25f1b6
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-08 11:53:29 +0530

    Multi Dimensional Arrays

--- DAY-14/first.cpp
+++ DAY-14/first.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+/*
+    1. Multidimensional Array
+        2-D array in the form of Matrix -> Rows and Columns
+
+    2. Declaration
+        <datatype> <array name> [<rows>] [<columns>]
+        EG: int mat[3][4];
+*/
+
 using namespace std;
 
 int main(){

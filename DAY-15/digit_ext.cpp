290caa5be3437d94be1c2ee7cd07b28c72bf4849
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-11 11:56:13 +0530

    Palindrome number

--- DAY-15/digit_ext.cpp
+++ DAY-15/digit_ext.cpp
@@ -3,7 +3,10 @@
 
 using namespace std;
 
+void palindrome(string num, int num1);
+
 void digit(int n, int size){
+    int num = n;
     string res = "";
     for(int i = 0; i < size; i++){
         int d = n%10;
@@ -11,9 +14,20 @@ void digit(int n, int size){
         res += to_string(d);
     }
     cout << res << endl;
+    palindrome(res, num);
+}
+
+void palindrome(string num, int num1){
+    string numStr = to_string(num1);
+
+    if(num == numStr){
+        cout << "Number is palindrome" << endl;
+    }else{
+        cout << "Number is not palindrome" << endl;
+    }
 }
 
 int main(){
-    digit(1234, 4);
+    digit(1221, 4);
     return 0;
 }

7fc32b48ad6b220b97878792e8de3a327f43b581
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-11 11:02:53 +0530

    Classes | User Def. Data types

---
+++ DAY-15/first.cpp
@@ -0,0 +1,12 @@
+// User defined Data-Types : Classes
+
+/*
+    Structure Syntax:
+        struct < structure name >
+        {
+            int roll_no;
+            int phone_no;
+            float marks;
+            char name[20];
+        }
+*/

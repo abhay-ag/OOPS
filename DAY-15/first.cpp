6e05bccfa9cde271d75d18f39bb963a0d6d80077
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-11 11:17:59 +0530

    Highest Salary using structures

--- DAY-15/first.cpp
+++ DAY-15/first.cpp
@@ -8,5 +8,43 @@
             int phone_no;
             float marks;
             char name[20];
-        }
+        };
 */
+#include <iostream>
+using namespace std;
+
+struct Student{
+    int rollNo;
+    long int phNo;
+    float marks;
+    string name;
+};
+
+struct Employee{
+    int empid;
+    char ename[50];
+    int salary;
+};
+
+int main(){
+    struct Student s1 = {1,9466480463, 98.4,"Abhay"};
+    cout << s1.name << endl;
+
+    Employee emps[3];
+
+    for (int i = 0 ; i < 3; i++){
+        cin >> emps[i].empid;
+        cin >> emps[i].ename;
+        cin >> emps[i].salary;
+    }
+
+    int bigSal = emps[0].salary;
+
+    for(int i = 0; i < 3; i++){
+        if(emps[i].salary > bigSal){
+            bigSal = emps[i].salary;
+        }
+    }
+    cout << "Biggest Salary: " << bigSal << endl;
+    return 0;
+}

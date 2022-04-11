e4875d9199e16791169ac3881d34e7a2d6131c40
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-11 11:30:16 +0530

    Classes | Name of Employee

--- DAY-15/first.cpp
+++ DAY-15/first.cpp
@@ -9,6 +9,9 @@
             float marks;
             char name[20];
         };
+
+    Classes:
+        1. By default members are private.
 */
 #include <iostream>
 using namespace std;
@@ -30,7 +33,7 @@ int main(){
     struct Student s1 = {1,9466480463, 98.4,"Abhay"};
     cout << s1.name << endl;
 
-    Employee emps[3];
+    Employee emps[3];               // Creating an array of structures
 
     for (int i = 0 ; i < 3; i++){
         cin >> emps[i].empid;
@@ -39,12 +42,21 @@ int main(){
     }
 
     int bigSal = emps[0].salary;
+    int minSal = emps[0].salary;
+    int indexMax = 0;
+    int indexMin = 0;
 
     for(int i = 0; i < 3; i++){
         if(emps[i].salary > bigSal){
             bigSal = emps[i].salary;
+            indexMax = i;
+        }
+        if(emps[i].salary < minSal){
+            minSal = emps[i].salary;
+            indexMin = i;
         }
     }
-    cout << "Biggest Salary: " << bigSal << endl;
+    cout << "Biggest Salary is of " << emps[indexMax].ename << ": " << bigSal << endl;
+    cout << "Smallest Salary is of " << emps[indexMin].ename << ": " << minSal << endl;
     return 0;
 }

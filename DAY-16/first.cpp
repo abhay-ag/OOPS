5888e8bd543f5fddb74be774bfb7e4c4c40f7382
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-12 14:46:18 +0530

    classes and nested functions

--- DAY-16/first.cpp
+++ DAY-16/first.cpp
@@ -12,7 +12,34 @@ void func2(){
     cout << "Hello Nested Function" << endl;
 }
 
+
+class Set{
+    int a, b;
+    public:
+    void setData(int n1, int n2){
+        a = n1;
+        b = n2;
+        largest();
+    }
+    int largest(){
+        if (a>b){
+            return a;
+        }
+        else{
+            return b;
+        }
+    }
+    void display(){
+        cout << "The largest number is: " << largest() << endl;
+    }
+};
 int main(){
+    /*
+        Encapsulation: enclosing of data members and methods in a class.
+    */
+    Set s1;
+    s1.setData(10,15);
+    s1.display();
     func1();
     return 0;
 }

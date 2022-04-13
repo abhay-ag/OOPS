07b4794a3175d75f0995850a12f7f44a0b4721a3
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-13 15:42:18 +0530

    constant members | problem 1

--- DAY-17/first.cpp
+++ DAY-17/first.cpp
@@ -2,6 +2,30 @@
 
 using namespace std;
 
+/*
+    Cosntant member functions:
+*/
+
+class Counter{
+    int count;
+
+    public:
+    void set(){
+        count = 0;
+    }
+    void inc(){
+        count ++;
+    }
+    void display(){
+        count += 2;         // prob: display function should not be able to increase the value of count
+        cout << count;
+    }
+};
+
 int main(){
+    // Constant member;
+
+    const int x = 10;
+    // x = x+10;        throws an error
     return 0;
 }

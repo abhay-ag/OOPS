13729bc212e003b893c69d25a0c2745eb7df6ec6
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-15 12:14:44 +0530

    Friend classes

--- DAY-18/first.cpp
+++ DAY-18/first.cpp
@@ -24,6 +24,33 @@ class Counter{
         }
 };
 
+class A{
+    int a;
+
+    protected:
+    int b;
+
+    public:
+    friend class B;
+    void set(int i, int j){
+        a = i;
+        b = j;
+    }
+};
+
+class B{
+    int x;
+
+    protected:
+    int y;
+
+    public:
+    void display(A obj){
+        cout << "a = " << obj.a << " b = " << obj.b << endl;            // throws an error as a and b can't be used after the end of scope of A.
+        // use friend class B; in class A
+        // vice versa not possible
+    }
+};
 int main(){
     Counter c1;         // non constant member can access all types of functions.
 
@@ -38,6 +65,17 @@ int main(){
     // c2.inc();
     c2.display();
 
+    /*
+        Friend Classes:
+        Problem:
+            Eg: class A{ a, b ==> a<private> b<public>};
+                class B{ A obj; obj.a; obj.b; // Shows error.};
+        Solution:
+            line: 27;
+    */
 
+    A obj1;B obj2;
+    obj1.set(10,20);
+    obj2.display(obj1);
     return 0;
 }

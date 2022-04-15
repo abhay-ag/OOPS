8986a5bb4d5130f2dd56f329d8c98efc18cd576e
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-15 12:01:25 +0530

    constant member functions | bit about constructors

--- DAY-18/first.cpp
+++ DAY-18/first.cpp
@@ -2,6 +2,42 @@
 
 using namespace std;
 
+// Constant Member Fucnctions Contd.:
+
+class Counter{
+    int count;
+
+    public: 
+
+    Counter(){          // constructor is to be made to avoid all the constant class member discrepancies
+        count = 0;
+    }
+        void set(){
+            count = 0;
+        }
+        void inc(){
+            count ++;
+        }
+        void display() const{
+            // count += 2;      throws an error
+            cout << count;
+        }
+};
+
 int main(){
+    Counter c1;         // non constant member can access all types of functions.
+
+    c1.set();
+    c1.inc();
+    c1.inc();
+    c1.display();
+
+    const Counter c2;   // constant members can access only constant member functions.
+
+    // c2.set();
+    // c2.inc();
+    c2.display();
+
+
     return 0;
 }

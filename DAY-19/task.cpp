2c6df0dc434596459953f3405a3ebb932d352006
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-18 11:31:32 +0530

    code described

--- DAY-19/task.cpp
+++ DAY-19/task.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-class B;
+class B;        // forward declaration of any of the classes that is declared after first class
 
 class A{
     int a;
 
     public:
-    friend void max(A,B);
+    friend void max(A,B);       // friend has to be declared in both the classes else it will not work.
     friend void min(A,B);
     friend float avg(A,B);
     void setData(int i){
@@ -28,9 +28,9 @@ class B{
     }
 };
 
-void max(A a, B b){
+void max(A a, B b){     // passing objects in the max functions
     if(a.a > b.b){
-        cout << a.a << " is greater. ";
+        cout << a.a << " is greater. ";     // if friend not delared then error will come
     }else{
         cout << b.b << " is greater. ";
     }
@@ -56,10 +56,10 @@ int main() {
 
     A a1; B b1;
 
-    a1.setData(a);
+    a1.setData(a);      // setting both the variables side by side
     b1.setData(b);
 
-    max(a1, b1);
+    max(a1, b1);        // calling the functions
     min(a1, b1);
     cout << avg(a1, b1);
 

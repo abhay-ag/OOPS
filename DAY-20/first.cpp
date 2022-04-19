6befa3555044a80f4e0687bdcdec37d1976c6eac
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-19 12:04:15 +0530

    more examples

--- DAY-20/first.cpp
+++ DAY-20/first.cpp
@@ -25,17 +25,27 @@ class Integer{
     int a, b;
 
     public: 
+
+// constructor overloading
+
     // 0 parameter constructor
     Integer(){
         a = 0;
         b = 0;
     }
-                                            // constructor overloading
+
     // parameterized constructor
     Integer(int i, int j){
         a = i;
         b = j;
     }
+
+    // Single paramter
+    Integer(int i){
+        a = b = i;
+    }
+
+    // Display function
     void display() const{
         cout << "A => " << a << " B => " << b << endl;
     }
@@ -44,10 +54,11 @@ class Integer{
 int main() {
     Hello h1;       // even without any method "Cunstructor fired" is printed onto the terminal.
 
-    Integer i1(10,20), i2(20,30), i3;
+    Integer i1(10,20), i2(20,30), i3 , i4(50);
 
     i1.display();
     i2.display();
     i3.display();       // constructor overloading
+    i4.display();
     return 0;
 }

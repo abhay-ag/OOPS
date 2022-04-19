298dc104f33f410904c7829595f55ec2ca6f6555
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-19 12:30:31 +0530

    time data type made

--- DAY-20/time.cpp
+++ DAY-20/time.cpp
@@ -10,13 +10,42 @@ class Time{
         h = hour; m = minutes; s = seconds;
     }
 
+    Time addtime(Time t1){
+        Time t3;
+        
+        t3.s = t1.s + s;
+        t3.m = t1.m + m;
+        t3.h = t1.h + h;
+
+        if (t3.s > 60){
+            t3.s -= 60;
+            t3.m += 1;
+        }
+        
+        if (t3.m > 60){
+            t3.m -= 60;
+            t3.h += 1;
+        }
+
+        if(t3.h > 24){
+            t3.h -= 24;
+        }
+
+        return t3;
+    }
+
     void disp() const{
         cout << h << ":" << m << ":" << s << endl;
     }
 };
 int main(){
-    Time t1(11,22,22);
+    Time t1(11, 26, 39), t2(14, 40, 40), t3;
     
     t1.disp();
+    t2.disp();
+
+    t3 = t1.addtime(t2);
+
+    t3.disp();
     return 0;
 }

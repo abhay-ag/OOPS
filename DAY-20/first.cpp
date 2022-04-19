50d58573a9c8956415f3310d7da6b0ece504f4a9
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-19 11:55:07 +0530

    about Constructors

--- DAY-20/first.cpp
+++ DAY-20/first.cpp
@@ -2,6 +2,24 @@
 
 using namespace std;
 
+/*
+    Constructor: 
+        1. Same name as the class name
+        2. Declared always in the public scope
+        3. no return type
+        4. Automatically invoked when objects of a class are created
+        5. Supports overloading
+*/
+
+class Hello{
+    public:
+    // fired even without calling
+    Hello(){
+        cout << "Cunstructor fired" << endl;
+    }
+};
+
 int main() {
+    Hello h1;       // even without any method Cunstructor fired. is printed onto the terminal.
     return 0;
 }

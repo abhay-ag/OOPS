a5350ce9be3bfe07e60beaa4ead735e6c7ad84c2
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-19 12:15:12 +0530

    Data type Made

--- DAY-20/imaginary.cpp
+++ DAY-20/imaginary.cpp
@@ -2,10 +2,44 @@
 using namespace std;
 
 class Complex{
+    int real, imag;
 
+    public:
+    friend Complex sum(Complex, Complex);
+
+    Complex(){
+        real = 0;
+        imag = 0;
+    }
+
+    Complex(int r, int i){
+        real = r;
+        imag = i;
+    }
+
+    void disp() const{
+        cout << real << "+" << imag << "i" << endl;
+    }
 };
 
+Complex sum(Complex ob1, Complex ob2){
+    Complex c3;
+
+    c3.real = ob1.real + ob2.real;
+    c3.imag = ob1.imag + ob2.imag;
+
+    return c3;
+}
+
 int main() {
+
+    Complex c1(5,7), c2(7,10), c3;
+
+    c3 = sum(c1, c2);
+
+    c1.disp();
+    c2.disp();
+    c3.disp();
 
     return 0;
 }

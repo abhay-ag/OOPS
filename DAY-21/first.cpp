c38564873dd570a1ca6b45ca2a832289dfbadfa7
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-20 15:47:10 +0530

    destructors

--- DAY-21/first.cpp
+++ DAY-21/first.cpp
@@ -14,21 +14,34 @@ class Point{
         x = 0;
         y = 0;
     }
-
     // const used to prevent from changing the values of x and y
     Point(const Point &p){            // call by refernce for copy constructor
         x = p.x;
         y = p.y;
     }
+    // destructor
+    ~Point(){
+        cout << "Program ended." << endl;
+    }
     void set(int i, int j){
         x = i;
         y = j;
     }
-
     void disp() const{
         cout << "X => " << x << " Y => " << y << endl;
     }
 };
+
+/*
+    DESTRUCTOR:
+        1. Called when an object dies.[pointer moved out of the object scope]
+        2. no return type
+
+    Syntax:
+        ~ <Class name>(){
+
+        }
+*/
 
 int main(){
     Point p1;

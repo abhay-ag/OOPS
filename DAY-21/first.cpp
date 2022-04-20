6bbca9a1b3af2029b9fe0cca7dc7521e5cdcf89a
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-20 15:39:39 +0530

    copy constructor working

--- DAY-21/first.cpp
+++ DAY-21/first.cpp
@@ -1,1 +1,41 @@
 // Copy constructor
+#include <iostream>
+
+using namespace std;
+
+class Point{
+    int x, y;
+    public:
+    // we want the values of p1 into p2
+    // for that we require the copy constructor
+
+    Point(){
+        x = 0;
+        y = 0;
+    }
+
+    Point(Point &p){            // call by refernce for copy constructor
+        x = p.x;
+        y = p.y;
+    }
+    void set(int i, int j){
+        x = i;
+        y = j;
+    }
+
+    void disp() const{
+        cout << "X => " << x << " Y => " << y << endl;
+    }
+};
+
+int main(){
+    Point p1;
+
+    p1.set(12, 10);
+
+    Point p2 = p1;
+
+    p1.disp();
+    p2.disp();
+    return 0;
+}

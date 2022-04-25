38740c6a1fb0e8559a317788fbd7c93ee990a789
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-25 11:50:47 +0530

    task completed

--- DAY-22/task.cpp
+++ DAY-22/task.cpp
@@ -9,6 +9,9 @@ class A{
 
     public:
     friend void swaping(A &ob1, B &ob2);
+    friend void min(A &ob1, B &ob2);
+    friend void max(A &ob1, B &ob2);
+    friend void mean(A &ob1, B &ob2);
     friend class B;
     A(int i = 0){
         a = new int;
@@ -24,6 +27,9 @@ class B{
 
     public:
     friend void swaping(A &ob1, B &ob2);
+    friend void min(A &ob1, B &ob2);
+    friend void max(A &ob1, B &ob2);
+    friend void mean(A &ob1, B &ob2);
     B(int j = 0){
         b = new int;
         *b = j;
@@ -39,12 +45,16 @@ class B{
     void min(A &ob1, B &ob2){
         if(ob1.a > ob2.b){
             cout << "B " << *ob2.b << " is minimum." << endl;
+        }else{
+            cout << "A " << *ob1.a << " is minimum." << endl;
         }
     }
 
     void max(A &ob1, B &ob2){
         if(ob1.a > ob2.b){
             cout << "A " << *ob1.a << " is maximum." << endl;
+        }else{
+            cout << "B " << *ob2.b << " is maximum." << endl;
         }
     }
 
@@ -58,6 +68,28 @@ class B{
         cout << "B after swap " << *b << endl;
     }
 };
+
+void min(A &ob1, B &ob2){
+    if(ob1.a > ob2.b){
+        cout << "B " << *ob2.b << " is minimum." << endl;
+    }else{
+        cout << "A " << *ob1.a << " is minimum." << endl;
+    }
+}
+
+void max(A &ob1, B &ob2){
+    if(ob1.a > ob2.b){
+        cout << "A " << *ob1.a << " is maximum." << endl;
+    }else{
+        cout << "B " << *ob2.b << " is maximum." << endl;
+    }
+}
+
+void mean(A &ob1, B &ob2){
+    int meanA = (*ob1.a + *ob2.b) /2;
+
+    cout << "Mean => " << meanA << endl;
+}
 
 void swaping(A &ob1, B &ob2){
     int *temp = ob1.a;
@@ -91,5 +123,9 @@ int main(){
 
     a1.disp();
     b1.disp();
+
+    min(a1,b1);
+    max(a1,b1);
+    mean(a1,b1);
     return 0;
 }

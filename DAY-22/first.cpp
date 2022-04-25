f9a1987c5aeeb838d777964fd943e52ded35e686
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-25 11:05:54 +0530

    Constructor created

--- DAY-22/first.cpp
+++ DAY-22/first.cpp
@@ -7,7 +7,26 @@
 #include <iostream>
 using namespace std;
 
+class Complex{
+    int real, imag;
+
+    public:
+
+    Complex(int i = 0, int j = 0){
+        real = i;
+        imag = j;
+    }
+
+    void disp() const{
+        cout << real << " + " << imag << "i" << endl;
+    }
+};
+
 int main() {
-    
+    // default arguements are optional, and are always declared at the last after all required arguments are declared.
+
+    Complex c1(5);
+
+    c1.disp();
     return 0;
 }

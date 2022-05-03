9e0744ef1788c5800900de93ba8413803fe17a13
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-03 14:51:39 +0530

    functions check

--- DAY-23/first.cpp
+++ DAY-23/first.cpp
@@ -25,9 +25,14 @@ class Counter{
 int main(){
     // object creation
     Counter c1, c2;
+    c1.inc();
+    c1.inc();
+    c2.dec();
 
+    c1.show();
+    c2.show();
     
-    // pre and post increment or decrement work only in in-built data-type;
+    // pre and post increment or decrement; or any other operator; work only in in-built data-type;
     // to use them with our data-types: we need to use operator overloading;
     return 0;
 }

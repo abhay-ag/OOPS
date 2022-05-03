12cca92e0ef685b039a295586e69827650f758c7
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-03 15:25:22 +0530

    operator overloading done on time.cpp

--- DAY-23/time-op-overload.cpp
+++ DAY-23/time-op-overload.cpp
@@ -36,18 +36,50 @@ class Time{
         return t3;
     }
 
+    // using operator overloading
+
+    Time operator + (Time t1){
+        // same logic but different definition.
+        Time t3;
+        
+        t3.s = t1.s + s;
+        t3.m = t1.m + m;
+        t3.h = t1.h + h;
+
+        if (t3.s > 60){     // checking for second greater than 60
+            t3.s -= 60;
+            t3.m += 1;
+        }
+        
+        if (t3.m > 60){      // checking for minutes greater than 60
+            t3.m -= 60;
+            t3.h += 1;
+        }
+
+        if(t3.h > 24){      // checking for hours greater than 24
+            t3.h -= 24;
+        }
+
+        return t3;
+    }
+
     void disp() const{
         cout << h << ":" << m << ":" << s << endl;
     }
 };
 int main(){
-    Time t1(11, 26, 39), t2(14, 40, 40), t3;        // creating three objects
+    Time t1(11, 26, 39), t2(11, 40, 40), t3;        // creating three objects
     
     t1.disp();
     t2.disp();
 
-    t3 = t1.addtime(t2);        // calling the addtime() function
+    // t3 = t1.addtime(t2);        // calling the addtime() function
 
-    t3.disp();      // displaying the result
+
+    // using operator overloading
+    Time resT = t1 + t2;
+    resT.disp();
+
+    // t3.disp();      // displaying the result
     return 0;
 }

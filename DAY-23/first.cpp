015ff97e9c03694e2552e4259cdeb090828cd3f8
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-03 14:55:39 +0530

    operator overloading done

--- DAY-23/first.cpp
+++ DAY-23/first.cpp
@@ -11,23 +11,43 @@ class Counter{
         count = 0;
     }
     // member functions
+    // =============== //
     void inc(){
         count++;
     }
     void dec(){
         count--;
     }
+    // =============== //
+    // operator overloading
+    
+    void operator ++(){
+        count++;
+    }
     void show() const{
         cout << "Counter: " << count << endl;
     }
 };
 
+/*
+    Syntax of operator overloading: [can be achieved through a friend function or a member function]
+
+    1. Member function:
+        <return type> operator <symbol>(parameters if any){
+            // logic
+        }
+*/
+
 int main(){
     // object creation
     Counter c1, c2;
-    c1.inc();
-    c1.inc();
-    c2.dec();
+    c1.inc();   // ++c1
+    c1.inc();   // ++c1
+    c2.dec();   // --c2
+
+
+    // operator overloading ==> increment
+    ++c1;
 
     c1.show();
     c2.show();

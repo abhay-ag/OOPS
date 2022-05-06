604dc17b3c016c6a5fa6ede3761fdc7c952115fb
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-06 13:58:31 +0530

    all bugs fixed | working correctly

--- DAY-24/distance-op-overload.cpp
+++ DAY-24/distance-op-overload.cpp
@@ -15,10 +15,19 @@ class Distance
         Distance temp;
         temp.feet = feet + d1.feet;
         temp.inches = inches + d1.inches;
-
-        if(temp.inches >= 12){
-            temp.inches -= 12;
-            temp.feet += 1;
+        
+        if(temp.inches % 12 == 0){
+            while (temp.inches != 0){
+                temp.inches -= 12;
+                temp.feet += 1;
+            }
+        }else{
+            while(!(temp.inches < 12)){
+                if(temp.inches >= 12){
+                    temp.inches -= 12;
+                    temp.feet += 1;
+                }
+            }
         }
 
         return temp;
@@ -30,8 +39,8 @@ class Distance
 };
 
 int main(){
-    Distance d1(5,12);
-    Distance d2(4,12);
+    Distance d1(5,11);
+    Distance d2(4,14);
 
     Distance d3 = d1 + d2;
 

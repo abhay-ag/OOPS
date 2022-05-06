2c8342e3a448a2d285524cf57cd404e6e511f7bd
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-06 13:52:32 +0530

    distances added  using op-overloading

--- DAY-24/distance-op-overload.cpp
+++ DAY-24/distance-op-overload.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 using namespace std;
 
+class Distance
+{
+    int feet, inches;
+    public:
+
+    Distance (int i = 0, int j = 0){
+        feet = i;
+        inches = j;
+    }
+
+    Distance operator + (Distance d1){
+        Distance temp;
+        temp.feet = feet + d1.feet;
+        temp.inches = inches + d1.inches;
+
+        if(temp.inches >= 12){
+            temp.inches -= 12;
+            temp.feet += 1;
+        }
+
+        return temp;
+    }
+
+    void disp() const{
+        cout << feet << " feet and " << inches << " inches." << endl;
+    }
+};
+
 int main(){
+    Distance d1(5,12);
+    Distance d2(4,12);
+
+    Distance d3 = d1 + d2;
+
+    d3.disp();
     return 0;
 }

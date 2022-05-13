a19deb8f98f5057704dfb5b086c032451387b76f
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-13 14:11:18 +0530

    inheritance example

--- DAY-25/first.cpp
+++ DAY-25/first.cpp
@@ -32,7 +32,47 @@ using namespace std;
         4. Hierarchical         A => B & C & D
         5. Hybrid               A => B => C & D  || mixture of more than two inheritence types
 */
-int main(){
 
+class Base{
+    int x;
+    protected:
+    int y;
+    public: 
+    int z;
+
+    void set_x(){
+        cout << "Enter x: ";
+        cin >> x;
+    }
+
+    void dispX(){
+        cout << "X => " << x << endl;
+    }
+};
+
+class Derived : Base{
+    public: 
+    void set(){
+        // to set x;
+        set_x();
+        // other members accessible
+        cout << "Enter y: ";
+        cin >> y;
+
+        cout << "Enter z: ";cin >> z;
+    }
+    void disp(){
+        //display x
+        dispX();
+        //other members accessible
+        cout << "Y => " << y << endl;
+        cout << "Z => " << z << endl;
+    }
+};
+
+int main(){
+    Derived d;
+    d.set();
+    d.disp();
     return 0;
 }

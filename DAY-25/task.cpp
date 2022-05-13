4348a893f48c32f6fc1c9282cc14af4d1f701ded
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-13 14:20:22 +0530

    task completed | student grade

---
+++ DAY-25/task.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class Student{
+    protected:
+    int m1, m2, m3;
+};
+
+class Test : Student{
+    int average;
+    public:
+    void get(){
+        cout << "Enter marks in three subjects: ";
+        cin >> m1 >> m2 >> m3;
+        avg();
+        grade();
+    }
+    private:
+    void avg(){
+        average = (m1 + m2 + m3) / 3;
+        cout << "Average: " << average << endl;
+    }
+    void grade(){
+        if(average > 90 && average <= 100){
+            cout << "Grade A" << endl;
+        }else if(average > 80 && average <= 90){
+            cout << "Grade B" << endl;
+        }else if(average > 70 && average <= 80){
+            cout << "Grade C" << endl;
+        }else if(average > 55 && average <= 70){
+            cout << "Grade D" << endl;
+        }else{
+            cout << "Grade E" << endl;
+        }
+    }
+
+};
+
+int main(){
+    Test s1;
+    s1.get();
+    return 0;
+}

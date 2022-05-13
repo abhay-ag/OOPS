43df0da8def0d00dddd24fbbd3ea245073d3b764
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-13 13:49:16 +0530

    about inheritence

--- DAY-25/first.cpp
+++ DAY-25/first.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
 
-// inheritance
+// inheritance ==> inheriting features from parent
+// creating a new class from existing class. ==> new class = child class && existing class = parent class
+// child class == derived class || sub class && parent class == base class || super class
 int main(){
 
     return 0;

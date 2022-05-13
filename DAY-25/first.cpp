6ba7b1ca177b7b05b9557eae75f9e5935d505cf9
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-13 13:54:04 +0530

    inheritence syntax | features

--- DAY-25/first.cpp
+++ DAY-25/first.cpp
@@ -4,6 +4,24 @@ using namespace std;
 // inheritance ==> inheriting features from parent
 // creating a new class from existing class. ==> new class = child class && existing class = parent class
 // child class == derived class || sub class && parent class == base class || super class
+/*
+    class A{
+        private:
+        int x;
+        protected:
+        int y;
+        public:
+        int z;
+    };
+
+    syntax of derived class:
+        class <Class name> : <visibility specifier ==> private by default || public || protected> <Parent class>
+
+    class B : A{
+        // can't access X ==> private
+        // can access Y, Z
+    }
+*/
 int main(){
 
     return 0;

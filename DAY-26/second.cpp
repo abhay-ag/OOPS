c7802958676d079a358bb3e6186bd67013b5a1c4
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-16 11:21:31 +0530

    Paramterized Constructor done in inheritance

--- DAY-26/second.cpp
+++ DAY-26/second.cpp
@@ -15,6 +15,28 @@ class Student{
 
 };
 
+class Test:public Student{
+    int m1, m2;
+    public:
+                                // we define the name of the class 
+                                // in the form of Constructor():Student(<name of variable to be passed in Base Class>)
+    Test(int m_1 = 0, int m_2 = 0, int r = 1):Student(r){
+        m1 = m_1;
+        m2 = m_2;
+    }
+
+    void show(){
+        Student::show();     // this will do recursion;
+        // to avoid
+        // one solution use different names
+        // other declare data member as protected
+        // third solution use the scope resolution with base class name
+        cout << "Marks are: " << m1 << " " << m2 << endl;
+    }
+};
+
 int main(){
+    Test t1(25,25);
+    t1.show();
     return 0;
 }

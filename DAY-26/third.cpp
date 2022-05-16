7d3dd3908f109a5644c3f81460ed91ba0cd2fca4
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-16 15:39:05 +0530

    bindings explained | eg of polymorphism

--- DAY-26/third.cpp
+++ DAY-26/third.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
 
+// polymorphism
+/*
+    Means: many forms
+
+    Compile Time => Static || can be achieved using function & operator overloading     \\ EARLY BINDING
+    Run Time => Dynamic    || can be achieved using Virtual Fuction                     \\ LATE BINDING
+
+    eg: in operator overloading the compiler binds the operator on the basis of definition
+*/
 int main(){
-    
+
     return 0;
 }

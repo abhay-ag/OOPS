29f4b7b80be6125e9bbc7cf96c6846446b651a0a
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-16 11:03:21 +0530

    Output Explanation | Constructor Execution Sequence

--- DAY-26/first.cpp
+++ DAY-26/first.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 using namespace std;
 
+// constructor and destrucor in inheritance
+
+// base class
+class Base{
+    public:
+    // constructor
+    Base(){
+        cout << "Base Class Constructor" << endl;
+    }
+
+    // destructor
+    ~Base(){
+        cout << "Base Class Destructor" << endl;
+    }
+};
+
+// derived class
+class Derived : public Base{
+    public: 
+    Derived(){
+        cout << "Derived Class Constructor" << endl;
+    }
+
+    ~Derived(){
+        cout << "Derived Class Destructor" << endl;
+    }
+};
+
 int main(){
+    Derived d;
+    /*
+        Output:
+            1. Base executes first and last execution is of base.
+            2. Child executes after and before the parent object dies.
+    */
     return 0;
 }

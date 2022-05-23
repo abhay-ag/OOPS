2e5b73dccccadada1755347223ee50a1feb7254d
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-23 11:06:13 +0530

    classes made | new and delete keywords

--- DAY-29/first.cpp
+++ DAY-29/first.cpp
@@ -6,6 +6,28 @@ using namespace std;
     Exception Handling
 */
 
+class Base{
+    public:
+    Base(){
+        cout << "Constructing Base" << endl;
+    }
+    ~Base(){
+        cout << "Destructing Base" << endl;
+    }
+};
+
+class Derived : public Base{
+    public:
+    Derived(){
+        cout << "Constructing Derived" << endl;
+    }
+    ~Derived(){
+        cout << "Destucting Derived" << endl;
+    }
+};
+
+// new and delete used to dynamically allocate memory during runtime
+
 int main(){
     return 0;
 }

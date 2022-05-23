2ae270ccd1922ae29fd815c1bc3265574637b224
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-23 11:41:19 +0530

    about exception handling || catch all block

--- DAY-29/second.cpp
+++ DAY-29/second.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 // exception handling ==> runtine anomalies
 // code compiles correctly but has issues in some test cases.
+// of two types: ASYNC => out of user bound      SYNC => in user bound
+// examples: divide by zero.
+// handling by try and catch blocks.
+
+// statement after the throw statement will never be executed
+// catch all block catch(...) syntax || multiple catch statements can be there for one throw; 
 
 int main(){
     return 0;

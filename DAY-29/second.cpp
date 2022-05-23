c8f13c1ab208f807a5f388f95e6bd922fd205503
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-23 11:27:31 +0530

    exception handling

---
+++ DAY-29/second.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+// exception handling ==> runtine anomalies
+// code compiles correctly but has issues in some test cases.
+
+int main(){
+    return 0;
+}

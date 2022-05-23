a42832dc850c177886e4b6cfd50abc2671f05f07
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-23 11:24:46 +0530

    problem and solution || wrong output || ambiguity explained

--- DAY-29/first.cpp
+++ DAY-29/first.cpp
@@ -11,10 +11,12 @@ class Base{
     Base(){
         cout << "Constructing Base" << endl;
     }
-    ~Base(){
+    virtual ~Base(){
         cout << "Destructing Base" << endl;
     }
 };
+
+// ambiguity ?? solves the diamond problem use virtual class in one of the inheritence || mainly in hybrid inheritance || A => D1 & D2 => B
 
 class Derived : public Base{
     public:
@@ -29,5 +31,8 @@ class Derived : public Base{
 // new and delete used to dynamically allocate memory during runtime
 
 int main(){
+    Derived *d = new Derived();         // dynamically allocated *d points to the memory address
+    Base *b = d;                        // no need to write a &d because it will become pointer to the pointer
+    delete b;                           // not the expected output we don't get the destructing derived in the terminal the solution is to make the destructor of the base class virtual
     return 0;
 }

54b374313ecf9eb41e82400d36829b7448348b50
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-24 14:49:03 +0530

    class templates || usage

--- DAY-30/first.cpp
+++ DAY-30/first.cpp
@@ -5,8 +5,11 @@ using namespace std;
 // instead of function overloading and repeating the same function we use a generic data type
 
 // same class can be used for character but has to be repeated
+template <class T>
+
 class Example{
-    int x;
+    // int x;
+    T x;        // generic data type || at the time of object creation we can specify the data type of T || has to be same as the name specified in the template;
     public:
     void set(){
         cin >> x;
@@ -16,6 +19,24 @@ class Example{
     }
 };
 
+// solution
+/*
+    syntax of template:
+
+    template <class T>      ==> where T is a generic type
+*/
+
 int main(){
+    // declaring objects
+    // <class name> <dtype><object name>
+    Example <int> e1;
+    Example <char>e2;
+    Example <float>e3;
+    e1.set();
+    e2.set();
+    e3.set();
+    e1.get();
+    e2.get();
+    e3.get();
     return 0;
 }

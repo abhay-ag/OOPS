380b68823cda4ef9ba84b5aea03047b55bbb2589
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-24 14:43:29 +0530

    problems in normal programming

--- DAY-30/first.cpp
+++ DAY-30/first.cpp
@@ -2,7 +2,19 @@
 using namespace std;
 
 // template ==> used for generic programming -> class template or function template
+// instead of function overloading and repeating the same function we use a generic data type
 
+// same class can be used for character but has to be repeated
+class Example{
+    int x;
+    public:
+    void set(){
+        cin >> x;
+    }
+    void get(){
+        cout << x;
+    }
+};
 
 int main(){
     return 0;

087f5c90a1772a75b4ebacd8e42212e28b947c77
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-27 14:00:22 +0530

    Components of STL

--- DAY-31/first.cpp
+++ DAY-31/first.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+// Standard Template Library || STL
+// contains inbuilt class and function templates
+/*
+    Components of STL:
+        1. Containers   ==> in built class templates [sequence [vector, list, deque], associative [set, multiset, map, multimap], Adaptive, [stack, queue]]
+        2. Algorithms   ==> in built function templates
+        3. Iterators    ==> pointers (to traverse any container or iterate over any container)
+*/
 int main(){
     return 0;
 }

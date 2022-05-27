efc0055213c22fb73c66850bdbdc6153449428ec
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-27 14:04:30 +0530

    Container -- vector

--- DAY-31/first.cpp
+++ DAY-31/first.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 // Standard Template Library || STL
@@ -10,5 +11,14 @@ using namespace std;
         3. Iterators    ==> pointers (to traverse any container or iterate over any container)
 */
 int main(){
+    int n;
+    vector <int> rNo;
+    for(int i = 0; i < 5; i++){
+        cin >> n;
+        rNo.push_back(n);
+    }
+    for(int i = 0; i<5; i++){
+        cout << rNo.at(i) << endl;
+    }
     return 0;
 }

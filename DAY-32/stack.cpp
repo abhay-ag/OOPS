fbbd4dfa3e33c8e448c76e364e6743ad6ef40dc2
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-30 11:22:36 +0530

    Stack implemented

---
+++ DAY-32/stack.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main(){
+    stack <int>s1;
+    s1.push(5);
+    s1.push(4);
+
+    while(!s1.empty()){
+        cout << s1.top() << endl;
+        s1.pop();
+    }
+    return 0;
+}
